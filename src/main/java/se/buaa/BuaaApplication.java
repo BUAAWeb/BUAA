531b4e3eecdbca827dddbe196b6a2a6882d6ff55
UnYan <415212688@qq.com>
2020-11-18 20:32:58 +0800

    'init'

---
+++ src/main/java/se/buaa/BuaaApplication.java
@@ -0,0 +1,14 @@
+package se.buaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuaaApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(BuaaApplication.class, args);
+    }
+//注释
+}
---
+++ src/test/java/se/buaa/BuaaApplicationTests.java
@@ -0,0 +1,13 @@
+package se.buaa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuaaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
