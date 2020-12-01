e8adf8f799f4feb79adc949399469014a226e4fb
kxw009988 <15261398617@163.com>
2020-12-01 10:29:02 +0800

    按接口返回格式封装了一个Result类专门用来返回数据

---
+++ src/main/java/se/buaa/Entity/Response/Result.java
@@ -0,0 +1,56 @@
+package se.buaa.Entity.Response;
+
+import java.io.Serializable;
+
+public class Result<T> implements Serializable {
+    private int code;
+    private String msg;
+    private T data;
+
+    public Result(int code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public Result(int code, String msg, T data) {
+        this.code = code;
+        this.msg =msg;
+        this.data = data;
+    }
+
+    public static Result Success() {
+        return new Result(200, "");
+    }
+
+    public static Result Success(Object data) {
+        return new Result(200, "", data);
+    }
+
+    public static Result Error(int code, String msg) {
+        return new Result(code, msg, null);
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
