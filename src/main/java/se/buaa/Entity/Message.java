be99d59264e8eaa461e6323d641043c365459f59
MindLostGuy <1575747105@qq.com>
2020-12-22 19:27:18 +0800

    消息需求变更

--- src/main/java/se/buaa/Entity/Message.java
+++ src/main/java/se/buaa/Entity/Message.java
@@ -9,12 +9,20 @@ public class Message {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Integer msgid;
-    @Column(name = "content")
-    public String content;
     @Column(name = "is_read")
     public boolean is_read;
     @Column(name = "date")
     public Date date;
     @Column(name = "userid")
     public Integer userid;
+    @Column(name = "objectName")
+    public String objectName;
+    @Column(name = "objectID")
+    public String objectID;
+    @Column(name = "type")
+    public String type;
+    @Column(name = "success")
+    public boolean success;
+    @Column(name = "reason")
+    public String reason;
 }
